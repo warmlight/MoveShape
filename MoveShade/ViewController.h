461c6cd37b7075722fdec41ccb4c7b8709d947b7
Your Name <462451377@qq.com>
2015-12-10 11:02:50 +0800

    Initial Commit

---
+++ MoveShade/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoveShade
+//
+//  Created by yiban on 15/12/10.
+//  Copyright © 2015年 yiban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MoveShade/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoveShade
+//
+//  Created by yiban on 15/12/10.
+//  Copyright © 2015年 yiban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
